5aac960f8b5ac0ad4c5e61d446cfa3198d7f18a3
id <id@IN0013386>
2021-07-08 11:50:31 +0900

    編集の備考欄に初期表示がされない…

--- src/main/java/com/example/demo/TrashController.java
+++ src/main/java/com/example/demo/TrashController.java
@@ -46,8 +46,9 @@ public class TrashController {
 	//編集
 		@RequestMapping("/list/edit")
 		public ModelAndView edit(
-				@RequestParam("CODE") int code,
+				@RequestParam(name = "code") int code,
 				ModelAndView mv) {
+
 			Task task = null;
 
 			Optional<Task> recode = taskRepository.findById(code);
@@ -63,17 +64,41 @@ public class TrashController {
 		}
 
 		//編集アクション
-		@PostMapping("/list/edit")
-		public ModelAndView edit1(ModelAndView mv) {
+		@PostMapping("/update")
+		public ModelAndView edit1(
+				@RequestParam(name = "code") int code,
+				@RequestParam(name = "name") String name,
+				@RequestParam(name = "user_id") int user_id,
+				@RequestParam(name = "dline") Date dline,
+				@RequestParam(name = "prt_num") int prt_num,
+				@RequestParam(name = "cg_code") int cg_code,
+				@RequestParam(name = "group_id") int group_id,
+				@RequestParam(name = "progress") int progress,
+				@RequestParam(name = "memo") String memo,
+				ModelAndView mv) {
+
+			Task task = new Task(code, name, user_id, dline, prt_num, cg_code, group_id, progress, memo, true);
+			taskRepository.saveAndFlush(task);
+
+
+			//空の表示用リストを生成
+			ArrayList<Task> list = new ArrayList<Task>();
+
+			//全てのタスクを取得
+			List<Task> taskList = taskRepository.findAll();
+
+			//ゴミ箱に入れていなければ、表示するリストに追加
+			for (Task task1 : taskList) {
+				if (task1.isTrash() == true) {
+					list.add(task1);
+				}
+			}
+
+			mv.addObject("list", list);
 
 			mv.setViewName("list");
 			return mv;
 		}
-
-
-
-
-
 
 	//ゴミ箱へ投げる
 	@RequestMapping("/list/trash")
@@ -108,7 +133,7 @@ public class TrashController {
 		//リストが空であれば、メッセージを表示
 		//リストの中身があれば、リストを表示
 		if (list.isEmpty() == true) {
-			mv.addObject("message", "ゴミ箱は空です");
+			mv.addObject("message", "リストは空です");
 		} else {
 			mv.addObject("list", list);
 		}
